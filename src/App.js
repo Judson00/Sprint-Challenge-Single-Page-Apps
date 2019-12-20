4bd4013f7657681f0c3e3ea0ff9f5f8db8f3aa92
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-20 12:08:09 -0500

    comonents cleaned up, react-router set up and working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React, { useState, useEffect } from 'react';
-
+import { Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
+import WelcomePage from './components/WelcomePage'; 
 import Header from './components/Header';
+import CharacterList from './components/CharacterList';
 import CharacterCard from './components/CharacterCard';
 import Page from './components/Pagination';
 import styled from 'styled-components';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-  const [people, setPeople] = useState ([]);
-  const [url, setUrl] = useState ('https://rickandmortyapi.com/api/character/');
-  const [nextUrl, setNextUrl] = useState ('');
-  const [prevUrl, setPrevUrl] = useState ('');
-
-  useEffect(() => {
-    axios
-    .get(url)
-    .then(response => {
-      console.log(response.data);
-      setPeople(response.data.results);
-      setNextUrl(response.data.next);
-      setPrevUrl(response.data.previous);
-    })
-    .catch(error => console.log({error}));
-  },[url]);
 
   const StyledDiv=styled.div`
     display: flex;
@@ -35,13 +19,16 @@ const App = () => {
   return (
     <StyledDiv className='App'>
       <Header />
-      {people.map((person, index) => (
-        <CharacterCard 
-          key={index}
-          person = {person}
-        />
-      ))}
-      <Page next={nextUrl} previous={prevUrl} url={setUrl} />
+      <Switch>
+        <Route exact path='/'>
+          <WelcomePage />
+        </Route>
+        <Route path='/characters'>
+          <CharacterList />
+        </Route>
+        
+        {/* <Page next={nextUrl} previous={prevUrl} url={setUrl} /> */}
+      </Switch>
     </StyledDiv>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [people, setPeople] = useState ([]);
+  const [url, setUrl] = useState ('https://rickandmortyapi.com/api/character/');
+  const [nextUrl, setNextUrl] = useState ('');
+  const [prevUrl, setPrevUrl] = useState ('');
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+    .get(url)
+    .then(response => {
+      console.log(response.data);
+      setPeople(response.data.results);
+      setNextUrl(response.data.next);
+      setPrevUrl(response.data.previous);
+    })
+    .catch(error => console.log({error}));
+  },[url]);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <Link to='/'>Home</Link>
+      {people.map((person, index) => (
+        <CharacterCard 
+          key={index}
+          person = {person}
+        />
+      ))}
     </section>
   );
 }

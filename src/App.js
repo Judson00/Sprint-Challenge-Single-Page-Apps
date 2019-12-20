f45163d19c315ca3c42b0c917d0f02be4e7d87eb
Robert J. Mitchell <robert.judson@outlook.com>
2019-12-20 11:41:19 -0500

    names displayed, beginning of page navigation set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
-import Header from "./components/Header.js";
+import React, { useState, useEffect } from 'react';
 
+import axios from 'axios';
 
-export default function App() {
+import Header from './components/Header';
+import CharacterCard from './components/CharacterCard';
+import Page from './components/Pagination';
+import styled from 'styled-components';
+
+const App = () => {
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+  const [people, setPeople] = useState ([]);
+  const [url, setUrl] = useState ('https://rickandmortyapi.com/api/character/');
+  const [nextUrl, setNextUrl] = useState ('');
+  const [prevUrl, setPrevUrl] = useState ('');
+
+  useEffect(() => {
+    axios
+    .get(url)
+    .then(response => {
+      console.log(response.data);
+      setPeople(response.data.results);
+      setNextUrl(response.data.next);
+      setPrevUrl(response.data.previous);
+    })
+    .catch(error => console.log({error}));
+  },[url]);
+
+  const StyledDiv=styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `
   return (
-    <main>
+    <StyledDiv className='App'>
       <Header />
-    </main>
+      {people.map((person, index) => (
+        <CharacterCard 
+          key={index}
+          person = {person}
+        />
+      ))}
+      <Page next={nextUrl} previous={prevUrl} url={setUrl} />
+    </StyledDiv>
   );
 }
+
+export default App;
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React from 'react';
+import { Card, CardHeader, CardBody,
+  CardTitle } from 'reactstrap';
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+const StyledDiv = styled.div`  
+  max-width: 250px;
+  min-width: 250px;
+  display: flex;
+  flex-direction: column;
+`
+
+
+const Cards = props  => {
+  console.log(props);
+  const { name } = props.person;
+  return (
+    <StyledDiv>
+      <Card>
+        <CardHeader>Name: {name}</CardHeader>
+      </Card>
+    </StyledDiv>
+  );
+  }
+
+export default Cards;
